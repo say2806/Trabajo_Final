39a9a32623c463feffb494bb9f0943c83ebbc88a
0Hellest0 <ivsalsher@gmail.com>
2025-11-27 16:04:12 -0700

    Update Python's_Generic_Rolplay_Game.py

--- Python's_Generic_Rolplay_Game.py
+++ Python's_Generic_Rolplay_Game.py
@@ -1,1 +1,14 @@
+import random
+import matplotlib
+import pygame
+import Tkinter
+class Characters:
+
+  Self.name = Nombre
+  Self.class = clase
+  Self.level = 1
+  Self.life = 100
+  Self.strengh = 10
+  Self.intelligence = 10
+  Self.inventory = []
 
